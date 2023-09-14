93eb576dc31212ae0c4cbed4b9440719665d8959
Jane Summey <beatrice.summey@mykenzie.snhu.edu>
2023-09-14 12:07:39 -0400

    Update to app.js on server side and package.JSON.

--- capstone/packages/server/app.js
+++ capstone/packages/server/app.js
@@ -1,25 +1,42 @@
-import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import mongoose from "mongoose";
-import { DB_URL } from "./config/db";
-import { API_URL, PORT } from "./config/app";
-import router from "./routes/index";
-import seedDatabase from './seedDatabase'
-import keys from './config/keys'
-
-mongoose
-  .connect(DB_URL)
-  .then(() => console.log("[Database] Connection established."))
-  .catch((err) => console.log("[Database] Connection failed: ", err));
+import bodyParser from "body-parser";
+import cors from "cors";
+import apiRouter from "./routes/index.js";
 
 const app = express();
 
+// Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(API_URL, router);
+app.use(bodyParser.json());
 
-app.listen(PORT, () =>
-  console.log(`[Server] Listening for requests at http://localhost:${PORT}`)
-);
+// Database connection setup
+const MONGODB_URI =
+  "mongodb+srv://jasummey:Abandon33@cluster0.af2sabl.mongodb.net/capstone?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// API routes setup
+app.use("/api", apiRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
