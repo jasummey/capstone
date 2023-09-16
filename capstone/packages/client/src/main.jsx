04de95b193e9e89aeae905d9c91d3132c20f3b85
Jane Summey <beatrice.summey@mykenzie.snhu.edu>
2023-09-15 23:06:13 -0400

    Update to add recipe detail component and route links in the main jsx page.

--- capstone/packages/client/src/main.jsx
+++ capstone/packages/client/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetail from './components/RecipeDetail.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
+      </Routes>
+
+    </Router>
+   
   </React.StrictMode>,
 )
