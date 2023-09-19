0447fea9232ab73f249cf4fa9fdbc5350f3c1b6c
Jane Summey <beatrice.summey@mykenzie.snhu.edu>
2023-09-19 13:25:05 -0400

    Push to solve backend connection issue---.

--- capstone/packages/server/config/app.js
+++ capstone/packages/server/config/app.js
@@ -1,4 +1,2 @@
-const PORT = process.env.PORT || 5173;
-const API_URL = process.env.API_URL || "/api";
-
-module.exports = { PORT, API_URL };
+export const API_URL = process.env.API_URL || "/api";
+export const PORT = process.env.PORT || 3001;
--- capstone/packages/server/models/index.js
+++ capstone/packages/server/models/index.js
@@ -1,3 +1,5 @@
-export { default as Recipe } from "./recipes";
-export { default as Comment } from "./comments";
-export { default as User } from "./users";
+import Recipe from "./recipes.js";
+import Comment from "./comments.js";
+import User from "./users.js";
+
+export { Recipe, Comment, User };
