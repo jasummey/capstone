503570f00dbe491f4b5f3ef6cee5260fb9dbe40f
Jane Summey <beatrice.summey@mykenzie.snhu.edu>
2023-09-20 13:10:29 -0400

    save changes.

--- capstone/vite.config.js
+++ capstone/vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { open: true },
+  build: {
+    rollupOptions: {
+      input: "../index.html",
+    },
+  },
 });
