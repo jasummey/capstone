a97c28d5423579da8a17b325aeaa54d243fa9ff1
Julia Jimenez <julia.jimenez@mykenzie.snhu.edu>
2023-09-23 22:08:31 -0700

    worked on sign up button

---
+++ packages/client/src/components/Register/register.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './register.css'
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your registration logic here
+    console.log('Registration Form Data:', formData);
+  };
+
+  return (
+    <div className = "signup">
+      <h2>Sign up</h2>
+      <form className = "input" onSubmit={handleSubmit}>
+        <div className='input'></div>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
